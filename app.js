17056bef2c6c1fb843f828751a6445c8530dd0e4
bgallen <bgallen24@gmail.com>
2022-07-04 18:09:55 -0700

    generate page code added

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//const profileDataArgs = process.argv.slice(2, process.argv.length);
+//console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-   profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+//const printProfileData = profileDataArr => {
+  // profileDataArr.forEach((profileItem) => console.log(profileItem));
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+//first arguement is file name, second is data that's being written, third handles errors
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
