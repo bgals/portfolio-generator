cd229d043ef58cec5e15cc8eb0a3785d2eb08974
bgallen <bgallen24@gmail.com>
2022-07-04 18:38:51 -0700

    inquirer installed

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-//const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+import inquirer from "inquirer"
 
-//const printProfileData = profileDataArr => {
-  // profileDataArr.forEach((profileItem) => console.log(profileItem));
-//};
+//const fs = require('fs');
 
-//printProfileData(profileDataArgs);
-const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
 //first arguement is file name, second is data that's being written, third handles errors
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw err;
+//fs.writeFile('./index.html', pageHTML, err => {
+  //if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
